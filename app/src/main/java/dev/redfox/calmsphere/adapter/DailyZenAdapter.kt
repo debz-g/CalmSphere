7a1da12ce4cd680a55a6b7edbd40e54c760e8b21
debz-g <debz.exe@gmail.com>
2024-01-22 23:35:21 +0530

    Extra code.

--- app/src/main/java/dev/redfox/calmsphere/adapter/DailyZenAdapter.kt
+++ app/src/main/java/dev/redfox/calmsphere/adapter/DailyZenAdapter.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
 import com.squareup.picasso.Picasso
 import dev.redfox.calmsphere.R
+import dev.redfox.calmsphere.databinding.ZenCardItemBinding
 import dev.redfox.calmsphere.models.ShareDataModel
 import dev.redfox.calmsphere.models.ZenDataModel
 
@@ -79,3 +80,112 @@ class DailyZenAdapter(
 
     }
 }
+
+
+//TODO Code for getting multiple view types in recycler view to add the end element at the end of the list
+
+//class DailyZenAdapter(
+//    var zenData: MutableList<ZenDataModel>
+//) : ListAdapter<ZenDataModel, RecyclerView.ViewHolder>(ZenDataComparator()) {
+//
+//    var onShareClick: ((ShareDataModel) -> Unit)? = null
+//    var onSaveClick: ((ZenDataModel) -> Unit)? = null
+//    var onReadArticleClick: ((String) -> Unit)? = null
+//
+//    companion object {
+//        const val NORMAL_VIEW = 1
+//        const val END_VIEW = 2
+//    }
+//
+//
+//    inner class DailyZenViewHolder(private val normalBinding: ZenCardItemBinding) :
+//        RecyclerView.ViewHolder(normalBinding.root) {
+//        fun bind(zenData: ZenDataModel) {
+//            if (zenData.articleUrl.isNotEmpty()) {
+//                normalBinding.btnReadArticle.let {
+//                    it.isVisible = true
+//                    it.setOnClickListener {
+//                        onReadArticleClick?.invoke(zenData.articleUrl)
+//                    }
+//                }
+//            } else {
+//                normalBinding.btnReadArticle.isVisible = false
+//            }
+//            normalBinding.tvQuote.text = zenData.themeTitle
+//            Picasso.get().load(zenData.dzImageUrl).into(normalBinding.ivQuote)
+//
+//            normalBinding.btnShare.setOnClickListener {
+//                val shareDate = ShareDataModel(
+//                    zenData.text,
+//                    zenData.author,
+//                    zenData.dzImageUrl
+//                )
+//                onShareClick?.invoke(shareDate)
+//            }
+//
+//            normalBinding.btnSave.setOnClickListener {
+//                onSaveClick?.invoke(zenData)
+//            }
+//        }
+//    }
+//
+//    inner class EndIconViewHolder(private val endBinding: CardEndIconLayoutBinding) :
+//        RecyclerView.ViewHolder(endBinding.root)
+//
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        return when (viewType) {
+//            NORMAL_VIEW -> DailyZenViewHolder(
+//                ZenCardItemBinding.inflate(
+//                    LayoutInflater.from(parent.context),
+//                    parent,
+//                    false
+//                )
+//            )
+//
+//            END_VIEW -> EndIconViewHolder(
+//                CardEndIconLayoutBinding.inflate(
+//                    LayoutInflater.from(
+//                        parent.context
+//                    ), parent, false
+//                )
+//            )
+//
+//            else -> throw IllegalArgumentException("invalid item type")
+//        }
+//    }
+//
+//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+//        val item = zenData[position]
+//        when (holder) {
+//            is DailyZenViewHolder -> {
+//                holder.bind(item)
+//            }
+//
+//            is EndIconViewHolder -> {
+//                //No Ops
+//            }
+//        }
+//
+//    }
+//
+//    override fun getItemViewType(position: Int): Int {
+//        return if (position == zenData.size) {
+//            END_VIEW
+//        } else {
+//            NORMAL_VIEW
+//        }
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return zenData.size + 1
+//    }
+//
+//    class ZenDataComparator : DiffUtil.ItemCallback<ZenDataModel>() {
+//        override fun areItemsTheSame(oldItem: ZenDataModel, newItem: ZenDataModel) =
+//            oldItem.uniqueId == newItem.uniqueId
+//
+//        override fun areContentsTheSame(oldItem: ZenDataModel, newItem: ZenDataModel) =
+//            oldItem == newItem
+//    }
+//}
